94c2435f4af9bd332a75716e26191d92445f17a2
Daniel Temme <dtemme@gmail.com>
2013-06-10 22:45:36 +0200

    refactor test

--- spec/hello_spec.js
+++ spec/hello_spec.js
@@ -6,18 +6,24 @@ describe("the main page", function() {
 
 		expect($.ajax).toHaveBeenCalledWith("/travel");
 	});
-	it("calls the template with the data", function() {
-		// this serves as an example why capturing callback functions to test sucks
-		var doneSpy = jasmine.createSpy('done'),
-			data = [{id:23}],
+	describe("the travel destinations done callback", function() {
+		var data = [{id:23}],
 			callback;
-		spyOn($, "ajax").andReturn({done: doneSpy});
-		spyOn(JST, "app/templates/destinations.hb");
-		app.loadDestinations();
-		callback = doneSpy.mostRecentCall.args[0];
+		beforeEach(function(){
+			// this serves as an example why capturing callback functions to test sucks
+			var doneSpy = jasmine.createSpy('done');
+			spyOn($, "ajax").andReturn({done: doneSpy});
+			app.loadDestinations();
+			callback = doneSpy.mostRecentCall.args[0];
 
-		callback(data);
-		
-		expect(JST["app/templates/destinations.hb"]).toHaveBeenCalledWith(data);
+		});
+		it("calls the template with the data", function() {
+			spyOn(JST, "app/templates/destinations.hb");
+
+			callback(data);
+			
+			expect(JST["app/templates/destinations.hb"]).toHaveBeenCalledWith(data);
+		});
+
 	});
 });
